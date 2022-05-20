053328ac330fe24ae4fbe2c4f4e98e0eefcfe2fd
MichaelFessler-Laptop <michaelfessler@gmx.at>
2022-05-20 17:48:52 +0200

    Finished basic framework

--- src/Board.java
+++ src/Board.java
@@ -6,7 +6,25 @@ public class Board {
     private Cell[][] fields;
     private int gen;
 
+    public Board(int height, int width) {
+        fields = new Cell[height][width];
+        gen = 1;
 
+        for(int i = 0; i < fields[0].length; i++) {
+            for(int j = 0; j < fields.length; j++) {
+                fields[j][i] =  new Cell(false);
+            }
+        }
+    }
+
+    public void run() throws InterruptedException {
+        while(true) {
+            //draw();
+            //update();
+            gen++;
+            Thread.sleep(100);
+        }
+    }
 
     public Cell[][] getFields() {
         return fields;
